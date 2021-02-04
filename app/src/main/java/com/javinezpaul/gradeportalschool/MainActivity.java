5313a7f917d328199e9ea1734e55a93dda028e67
steven123francisco <steven123francisco@gmail.com>
2021-02-05 01:13:02 +0800

    onload , school , regform teacher
    onload , school , regform teacher

--- app/src/main/java/com/javinezpaul/gradeportalschool/MainActivity.java
+++ app/src/main/java/com/javinezpaul/gradeportalschool/MainActivity.java
@@ -2,13 +2,36 @@ package com.javinezpaul.gradeportalschool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button RegisterTeacher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RegisterTeacher = findViewById(R.id.RegisterTeacher);
+
+        RegisterTeacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent teacherReg = new Intent(MainActivity.this,RegisterTeacher.class);
+                startActivity(teacherReg);
+            }
+        });
+
+
+
+
+
+
+
+
+
     }
 }

fb20567312d702636bf8b18db84be616ab745f70
AngelicoGomez <nic1_01@yahoo.com>
2021-02-05 21:42:12 -0800

    resolving error

---
+++ app/src/main/java/com/javinezpaul/gradeportalschool/TeacherMainScreen.java
@@ -0,0 +1,14 @@
+package com.javinezpaul.gradeportalschool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeacherMainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_main_screen);
+    }
+}
---
+++ app/src/main/java/com/javinezpaul/gradeportalschool/TeacherSubjects.java
@@ -0,0 +1,14 @@
+package com.javinezpaul.gradeportalschool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeacherSubjects extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_subjects);
+    }
+}

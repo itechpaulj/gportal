9a8eac6139f18a761c92bd48d3d93e4eb628d5c1
steven123francisco <steven123francisco@gmail.com>
2021-02-07 22:06:03 +0800

    tes
    tes

--- app/src/main/java/com/javinezpaul/gradeportalschool/Testinganbutton.java
+++ app/src/main/java/com/javinezpaul/gradeportalschool/Testinganbutton.java
@@ -2,13 +2,41 @@ package com.javinezpaul.gradeportalschool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Testinganbutton extends AppCompatActivity {
 
+    Button patungko,pagumaoc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testinganbutton);
+
+
+
+        patungko = findViewById(R.id.patungko);
+        pagumaoc = findViewById(R.id.pagumaoc);
+
+        patungko.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent patungko = new Intent(Testinganbutton.this , Login.class);
+                startActivity(patungko);
+            }
+        });
+
+
+        pagumaoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pagumaoc = new Intent(Testinganbutton.this , Login2.class);
+                startActivity(pagumaoc);
+            }
+        });
+
     }
 }

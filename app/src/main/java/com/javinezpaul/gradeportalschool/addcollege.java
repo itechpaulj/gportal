a1cd33a3c5a674caae9333d942da59595c6f8cf6
Paul Javinez <itechpaulj@gmail.com>
2021-02-07 21:36:47 +0800

    androidManifest

--- app/src/main/java/com/javinezpaul/gradeportalschool/addcollege.java
+++ app/src/main/java/com/javinezpaul/gradeportalschool/addcollege.java
@@ -3,15 +3,18 @@ package com.javinezpaul.gradeportalschool;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 public class addcollege extends AppCompatActivity {
 
 class GradesAll extends AppCompatActivity {
     TextView viewallgrade;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addcollege);
     }
+}
 }

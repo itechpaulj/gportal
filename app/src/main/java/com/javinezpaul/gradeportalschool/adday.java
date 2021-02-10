5a4408e5ea0533c7576b41667c03132b10b31a72
RyeAnn <62006343+RyeAnn@users.noreply.github.com>
2021-02-09 19:53:11 -0800

    revised sub, AY, college, sec, prog

--- app/src/main/java/com/javinezpaul/gradeportalschool/adday.java
+++ app/src/main/java/com/javinezpaul/gradeportalschool/adday.java
@@ -6,35 +6,41 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class adday extends AppCompatActivity {
-    Spinner sem,year1,year2,level;
+    Spinner spinnerSem,spinnerYear1,spinnerYear2,spinnerLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adday);
 
-        sem =(Spinner)findViewById(R.id.sem);
-        year1 =(Spinner)findViewById(R.id.year1);
-        year2 =(Spinner)findViewById(R.id.year2);
-        level =(Spinner)findViewById(R.id.level);
+        spinnerSem =findViewById(R.id.spinnerSem);
+        spinnerYear1=findViewById(R.id.spinnerYear1);
+        spinnerYear2 =findViewById(R.id.spinnerYear2);
+        spinnerLevel =findViewById(R.id.spinnerLevel);
 
 
+        String[] sem = {"Sem","Sem","Sem"};
+        ArrayList<String> Sem = new ArrayList<>(Arrays.asList(sem));
+        ArrayAdapter<String> arrayAdaptersem = new ArrayAdapter<>(this,R.layout.style_spinner,Sem);
+        spinnerSem.setAdapter(arrayAdaptersem);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.sem, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sem.setAdapter(adapter);
+        String[] year1 = {"Year1","Year1","Year1"};
+        ArrayList<String> Year1 = new ArrayList<>(Arrays.asList(year1));
+        ArrayAdapter<String> arrayAdapterYear1 = new ArrayAdapter<>(this,R.layout.style_spinner,Year1);
+        spinnerYear1.setAdapter(arrayAdapterYear1);
 
-        ArrayAdapter<CharSequence> adapteryr= ArrayAdapter.createFromResource(this, R.array.yr, android.R.layout.simple_spinner_item);
-        adapteryr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        year1.setAdapter(adapteryr);
+        String[] year2 = {"Year2","Year2","Year2"};
+        ArrayList<String> Year2 = new ArrayList<>(Arrays.asList(year2));
+        ArrayAdapter<String> arrayAdapterYear2 = new ArrayAdapter<>(this,R.layout.style_spinner,Year2);
+        spinnerYear2.setAdapter(arrayAdapterYear2);
 
-        ArrayAdapter<CharSequence> adapteryear = ArrayAdapter.createFromResource(this, R.array.year, android.R.layout.simple_spinner_item);
-        adapteryear.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        year2.setAdapter(adapteryear);
-
-        ArrayAdapter<CharSequence> adapterlevel = ArrayAdapter.createFromResource(this, R.array.lvl, android.R.layout.simple_spinner_item);
-        adapterlevel.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        level.setAdapter(adapterlevel);
+        String[] lvl= {"Level","Level","Level"};
+        ArrayList<String> Lvl = new ArrayList<>(Arrays.asList(lvl));
+        ArrayAdapter<String> arrayAdapterlevel = new ArrayAdapter<>(this,R.layout.style_spinner,Lvl);
+        spinnerLevel.setAdapter(arrayAdapterlevel);
     }
 }

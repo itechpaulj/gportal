b6f704c96ec229873b6402300801d65849215dbf
Allan Adan <allanadan1999@gmail.com>
2021-02-10 22:53:27 +0800

    ViewSections, ViewAY Functionalities

--- app/src/main/java/com/javinezpaul/gradeportalschool/ViewAy.java
+++ app/src/main/java/com/javinezpaul/gradeportalschool/ViewAy.java
@@ -6,6 +6,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 
@@ -14,6 +25,9 @@ public class ViewAy extends AppCompatActivity {
     private RecyclerView ayRecView;
     private AyRecViewAdapter adapter;
     TextView toolbarName;
+    private String schoolcode;
+
+    ArrayList<Ay> ays = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +37,55 @@ public class ViewAy extends AppCompatActivity {
         toolbarName=findViewById(R.id.toolbarName);
         toolbarName.setText("Academic Year");
 
-        ayRecView = findViewById(R.id.ayRecView);
-        adapter = new AyRecViewAdapter(this);
+        SchoolMainScreen mainClass = new SchoolMainScreen();
+        schoolcode=mainClass.schoolcode;
+        Toast.makeText(getApplicationContext(), "Schoolcode: " + schoolcode + " is from SchoolMainScreen", Toast.LENGTH_LONG).show();
 
-        ayRecView.setAdapter(adapter);
-        ayRecView.setLayoutManager(new LinearLayoutManager(this));
+        getFunctionValley();
+    }
 
-        //to add data in arraylist
-        ArrayList<Ay> ays = new ArrayList<>();
-        ays.add(new Ay("1", "1st SEM", "2020", "2021", "1st Year"));
-        ays.add(new Ay("2", "2nd SEM", "2020", "2021", "1st Year"));
-        ays.add(new Ay("3", "1st SEM", "2021", "2022", "2nd Year"));
-        ays.add(new Ay("4", "2nd SEM", "2021", "2022", "2nd Year"));
+    private void getFunctionValley() {
+        RequestQueue queueu = Volley.newRequestQueue(this);
+        String url = "http://jeepcard.net/gportal/ay.php?schoolcode="+schoolcode;
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+//                collegeCounterTextView.setText("Response Get: "+response);
+//                Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+                ayRecView = findViewById(R.id.ayRecView);
+                adapter = new AyRecViewAdapter(ViewAy.this);
 
-        adapter.setAys(ays);
+                ayRecView.setAdapter(adapter);
+                ayRecView.setLayoutManager(new LinearLayoutManager(ViewAy.this));
+
+                try {
+                    JSONArray JA= new JSONArray(response);
+                    for(int i=0;i<JA.length();i++) {
+                        //receive JSON response to variables
+                        String ayid=JA.getJSONObject(i).get("ayid").toString();
+                        String ayear1=JA.getJSONObject(i).get("ayear1").toString();
+                        String ayear2=JA.getJSONObject(i).get("ayear2").toString();
+                        String ayearlevel=JA.getJSONObject(i).get("ayearlevel").toString();
+                        String aysem=JA.getJSONObject(i).get("aysem").toString();
+
+
+                        //to add data in arraylist
+                        ays.add(new Ay(ayid, aysem, ayear1, ayear2, ayearlevel));
+                    }
+                    adapter.setAys(ays);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            } //onResponse
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+//                Toast.makeText(getApplicationContext(), "Reconnecting", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Resolving error", Toast.LENGTH_LONG).show();
+                getFunctionValley();
+            }
+        });//Stringrequest
+        queueu.add(stringRequest);
     }
 }

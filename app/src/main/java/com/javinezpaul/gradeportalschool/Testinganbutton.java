ab2b1027ef35ffb56d8d1401e734181aca6a7591
steven123francisco <steven123francisco@gmail.com>
2021-02-20 16:42:34 +0800

    teacher main screen
    teacher main screen

--- app/src/main/java/com/javinezpaul/gradeportalschool/Testinganbutton.java
+++ app/src/main/java/com/javinezpaul/gradeportalschool/Testinganbutton.java
@@ -51,7 +51,7 @@ public class Testinganbutton extends AppCompatActivity {
         patungko.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent patungko = new Intent(Testinganbutton.this , Login.class);
+                Intent patungko = new Intent(Testinganbutton.this , StudentMianScreen.class);
                 startActivity(patungko);
 
             }
